fe4407edd50a359b9f1f3782012def24fe9bea0e
KnlnKS <soulkks@hotmail.ca>
2021-10-31 12:44:15 -0400

    ✨ Add DB Create + Find Routes

---
+++ website/pages/api/users/[firebaseId].ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { User } from "../../../types/models";
+import dbConnect from "../../../utils/dbConnect";
+import UserModel from "../../../models/User";
+
+type Data = {
+  success: boolean;
+  data?: User;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const {
+    method,
+    query: { firebaseId },
+  } = req;
+
+  if (method === "GET") {
+    await dbConnect();
+
+    try {
+      const user = await UserModel.findOne({
+        firebaseId: firebaseId,
+      });
+      if (!user) {
+        res.status(404).json({ success: false });
+      } else res.status(200).json({ success: true, data: user?.toObject() });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  } else res.status(400).json({ success: false });
+}
---
+++ website/pages/api/users/create.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { User } from "../../../types/models";
+import dbConnect from "../../../utils/dbConnect";
+import UserModel from "../../../models/User";
+
+type Data = {
+  success: boolean;
+  data?: User;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { method } = req;
+
+  if (method === "POST") {
+    await dbConnect();
+
+    try {
+      const user: User = await UserModel.create(req.body);
+      res.status(200).json({ success: true, data: user });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  } else res.status(400).json({ success: false });
+}
