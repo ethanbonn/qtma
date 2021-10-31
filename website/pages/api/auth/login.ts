2b5cbdc61f4c447e326718eb20b1d986f0bed2bb
KnlnKS <soulkks@hotmail.ca>
2021-10-31 12:41:48 -0400

    🎨 Add Type Import Declaration

--- website/pages/api/auth/login.ts
+++ website/pages/api/auth/login.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { setAuthCookies } from "next-firebase-auth";
 import initAuth from "../../../utils/initAuth";
 
