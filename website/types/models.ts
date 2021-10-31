b45a59d3d8d7e88d31e096df947090c3963df1c2
KnlnKS <soulkks@hotmail.ca>
2021-10-31 12:42:25 -0400

    🗃️ Make DB Model

---
+++ website/types/models.ts
@@ -0,0 +1,12 @@
+export interface User {
+  email: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  profilePicture: string;
+  jobTitle: string;
+  userDescription: string;
+  links: string[];
+  interests: string[];
+  firebaseId: string;
+}
