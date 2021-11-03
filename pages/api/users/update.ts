23e6fa7745cfbb9e41c2b65e0a08a5dec76fef13
KnlnKS <soulkks@hotmail.ca>
2021-11-03 18:18:20 -0400

    🛂 Add Proper Authorization Protection to Routes

--- pages/api/users/update.ts
+++ pages/api/users/update.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { verifyIdToken } from "next-firebase-auth";
+
 import type { User } from "../../../types/models";
 import dbConnect from "../../../utils/dbConnect";
 import UserModel from "../../../models/User";
@@ -15,13 +17,18 @@ export default async function handler(
   const { method } = req;
 
   if (method === "POST") {
-    await dbConnect();
-
     try {
       if (!req?.headers?.authorization) {
         throw new Error("No authorization token");
       }
+      await verifyIdToken(req?.headers?.authorization);
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
 
+    await dbConnect();
+
+    try {
       const updatedUser: User = await UserModel.findByIdAndUpdate(
         req?.body?._id,
         req?.body
