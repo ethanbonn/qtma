46307e665eaba63ee9ce9806f3ed5d1b2f41d6bc
RonanAlmeida <ronancalmeida@hotmail.com>
2021-11-28 16:48:20 -0500

    profile and projects

--- components/LandingPage/QueryCard.tsx
+++ components/LandingPage/QueryCard.tsx
@@ -1,18 +1,25 @@
 export default function Card() {
   return (
-    <div class="p-6 m-10 max-w-sm mx-auto bg-white rounded-xl shadow-md flex  space-x-4">
-      <div class="grid justify-items-center">
-        <div>
+    <div class=" p-4 m-10 max-w-auto mx-auto bg-white rounded-xl shadow-lg flex  ">
+      <div class="flex flex-wrap content-center">
+        <div class="mx-2">
           <div class="text-xl font-medium text-black">I'm looking for </div>
           <input class="my-3 border border-black rounded-lg  px-1 " />
         </div>
-        <div>
+        <div class="mx-2">
           <div class="text-xl font-medium text-black">that are skilled in </div>
           <input class="my-3 border border-black rounded-lg  px-1 " />
         </div>
-        <div>
+        <div class="mx-2">
           <div class="text-xl font-medium text-black">to build </div>
           <input class="my-3 border border-black rounded-lg  px-1 " />
+        </div>
+
+        <div>
+        <div class="text-xl font-medium text-black"> </div>
+          <button class="px-3 py-1 mt-9 text-green-100 bg-green-500 rounded-lg">
+            Search
+          </button>
         </div>
       </div>
     </div>
--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -1,33 +1,84 @@
 import Link from "next/link";
-import Card from "./Card"
+import PreviewCard from "./PreviewCard";
+import ProjectCard from "./ProjectCard";
+import ProfileCard from "./ProfileCard";
+
+import Card from "./QueryCard";
 export default function LandingPage() {
   return (
     <div>
       <nav class="container flex justify-around py-3 mx-auto bg-gradient-to-b from-green-300 to-green-400">
         <div class="flex items-center">
-          <h3 class="text-2xl font-medium text-white">Soar</h3>
+          <h3 class="font-sans  text-2xl font-medium text-white">Soar</h3>
         </div>
         <div class="items-center hidden space-x-8 lg:flex">
-          <h2 class="text-1xl font-medium text-white">Explore</h2>
+          <h3 class="font-sans  text-1xl font-medium text-white">Explore</h3>
         </div>
         <div class="flex items-center space-x-2">
           <Link href="/login">
-            <button class="px-4 py-2 text-green-100 bg-green-500 rounded-md">
+            <button class=" font-sans  px-4 py-2 text-white bg-green-600 rounded-md shadow-md">
               Sign in
             </button>
           </Link>
         </div>
       </nav>
 
-      <div class="container bg-green-400 py-40">
-        <div class="grid justify-items-center">
-          <p class="font-sans text-4xl font-large text-white px-3">
-            Build the dream team to soar your ideas to the moon 🚀
+      <div class="container bg-green-400 py-20">
+        <div class="grid justify-items-center mx-30">
+          <p class="font-sans text-4xl font-bold  text-white px-3">
+            Build the dream team to soar your ideas to the moon
           </p>
 
-          <Card/>
-        </div>
+          <Card />
+          <div class="flex flex-wrap content-center">
+            <PreviewCard />
+            <PreviewCard />
 
+            <PreviewCard />
+          </div>
+        </div>
+      </div>
+      <div class="container bg-gray-100 py-40">
+        <div class="grid justify-items-center mx-30">
+          <p class="font-sans text-3xl font-bold  text-blue-500 px-20">
+            Discover hundreds of projects to build, and connect with other
+            brightminded individuals{" "}
+          </p>
+
+          <div class="pt-20 px-20">
+            <p class="font-sans text-3xl font-bold  text-black ">
+              Featured Projects
+            </p>
+            <div class="flex flex-wrap content-center">
+              <ProjectCard />
+              <ProjectCard />
+
+              <ProjectCard />
+            </div>
+          </div>
+          <button class=" font-sans  px-4 py-2 text-white bg-green-500 rounded-full shadow-md">
+            Explore more projects{" "}
+          </button>
+
+          <div class="pt-20 px-20">
+            <p class="font-sans text-3xl font-bold  text-black ">Profiles</p>
+            <div class="flex flex-wrap content-center">
+              <ProfileCard />
+              <ProfileCard />
+
+              <ProfileCard />
+            </div>
+          </div>
+          <button class=" font-sans  px-4 py-2 text-white bg-green-500 rounded-full shadow-md">
+            Explore more profiles{" "}
+          </button>
+        </div>
+      </div>
+
+      <div class="container bg-gray-300 py-10">
+        <div class="grid justify-items-center mx-30">
+          <p class="font-sans text-md    px-20">Made with ❤️ by Team 4</p>
+        </div>
       </div>
     </div>
   );
