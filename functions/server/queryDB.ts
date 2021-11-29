127014c3b3f62a796ba2e161b1e4bf9797e9fb7b
Truman Be <trumanbe01@gmail.com>
2021-11-28 22:26:21 -0500

    Fixed some querying error checking, and added some commenting

--- functions/server/queryDB.ts
+++ functions/server/queryDB.ts
@@ -4,33 +4,34 @@ export type supported_timezones = "ACST" | "AEST" | "AKST" | "AST" | "AWST" | "C
 
 const queryDB = async (relationship?: "sponsor" | "collaborator", tags?: [string], skills?: [string], timezone?: [supported_timezones]): Promise<Project[] | null> => {
 
+    // Type guard for the query parameters
     type queryParams = {
         author_timezone: supported_timezones | supported_timezones[] | undefined;
         desired_relationship_type: "sponsor" | "collaborator" | undefined;
         project_tags: string | string[] | undefined;
-        skill_id: string | string[] | undefined;
+        skills: string | string[] | undefined;
     };
     
+    // Set the value of an object to the provided parameters to the function
     var params: queryParams = {
         author_timezone: timezone,
         desired_relationship_type: relationship,
         project_tags: tags,
-        skill_id: skills
+        skills: skills
     };
 
-    console.log(params);
-
+    // Create a separate array containing all valid parameters
     var valid_params: Array<String | String[]> = [];
 
+    // Map each parameter to the valid_params list if they are not undefined
     Object.keys(params).forEach(key => {
         if (params[key] !== undefined) {
             valid_params.push(key + "=" + params[key]);
         }
     });
 
-    var query_string = valid_params !== undefined ? valid_params.join("&") : "";
-
-    if (query_string === "") query_string = "query";
+    // If there are valid parameters, join them with &, otherwise, set the query string to query
+    var query_string = valid_params !== undefined ? valid_params.join("&") : "query";
 
     await fetch(`http://localhost:3000/api/projects/${query_string}`)
         .then((response) => response.json())
--- pages/api/projects/[query].ts
+++ pages/api/projects/[query].ts
@@ -17,24 +17,40 @@ export default async function handler(
     query
   } = req;
 
+  // Retrieve the search parameters from the URL
   let search_params = new URLSearchParams(query.query);
 
+  // Type guard for the URl query parameters
   type ProjectQuery = {
     author_timezone: string | string[] | undefined;
     desired_relationship_type: string | null;
     project_tags: string | string[] | undefined;
-    skill_id: string | string[] | undefined;
+    skills: string | string[] | undefined;
   };
 
+  // Set the value of an object to the provided URL query parameters
   const query_params: ProjectQuery = {
     "author_timezone": search_params.get("author_timezone")?.split(","),
     "desired_relationship_type": search_params.get("desired_relationship_type"),
     "project_tags": search_params.get("project_tags")?.split(","),
-    "skill_id": search_params.get("skill_id")?.split(",")
+    "skills": search_params.get("skills")?.split(",")
   };
 
+  // Initialize some variables:
+  //  all_params will be used for querying MongoDB using logical or operator
+  //  obj will be used as a temporary object
+  var all_params = {"$or": []};
+  var obj;
+
+  // Append each valid query to the logical or operation for querying in all_params
   for (const [key, value] of Object.entries(query_params)) {
-    if (value === undefined || value === null) delete query_params[key];
+    if (value === undefined || value === null)
+      delete query_params[key];
+    else {
+      obj = {};
+      obj[key] = value;
+      all_params.$or.push(obj);
+    }
   }
 
   if (method === "GET") {
@@ -42,7 +58,7 @@ export default async function handler(
     await dbConnect();
 
     try {
-      const queryobj = await ProjectModel.find(query_params);
+      const queryobj = await ProjectModel.find(all_params);
       if (!queryobj) throw new Error("Data not found");
       res.status(200).json({ success: true, data: queryobj });
     } catch (error) {
