dec1f8213f7871f3828dcadb9416e1f8f76489f8
RonanAlmeida <ronancalmeida@hotmail.com>
2021-12-05 14:59:26 -0500

    disabled es lint for now

--- components/Cards/ProjectCard.tsx
+++ components/Cards/ProjectCard.tsx
@@ -1,25 +1,25 @@
 export default function ProjectCard() {
   return (
-    <div class=" max-w-auto mx-10 bg-white rounded-xl shadow-lg flex m-10 ">
-      <div class="max-w-sm  rounded-xl  overflow-hidden shadow-lg">
-        <img class="w-80" src="https://api.time.com/wp-content/uploads/2020/06/crystalball.jpg?w=800&quality=85" alt="project" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2 text-blue-normal">Omicron Predictor</div>
-          {/* <p class="text-gray-700 text-base">
+    <div className=" max-w-auto mx-10 bg-white rounded-xl shadow-lg flex m-10 ">
+      <div className="max-w-sm  rounded-xl  overflow-hidden shadow-lg">
+        <img className="w-80" src="https://api.time.com/wp-content/uploads/2020/06/crystalball.jpg?w=800&quality=85" alt="project" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2 text-blue-normal">Omicron Predictor</div>
+          {/* <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, Nonea! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
           </p> */}
-          <p class="text-gray-500 font-normal">Desired Skills:</p>
+          <p className="text-gray-500 font-normal">Desired Skills:</p>
         </div>
-        <div class="px-6  pb-2">
-          <span class="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+        <div className="px-6  pb-2">
+          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
             Python
           </span>
-          <span class="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
             Data Analysis
           </span>
-          <span class="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
             Knime
           </span>
         </div>
--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -8,59 +8,59 @@ export default function LandingPage() {
   return (
     <div>
       <NavBar />
-      <div class=" bg-green-normal py-20">
-        <div class="grid justify-items-center mx-30">
-          <p class="font-nunito text-4xl font-bold  text-white px-3">
+      <div className=" bg-green-normal py-20">
+        <div className="grid justify-items-center mx-30">
+          <p className="font-nunito text-4xl font-bold  text-white px-3">
             Build the dream team to soar your ideas to the moon.
           </p>
           <Card />
-          <div class="flex flex-wrap content-center">
+          <div className="flex flex-wrap content-center">
             <PreviewCard />
             <PreviewCard />
             <PreviewCard />
           </div>
         </div>
       </div>
-      <div class=" bg-white py-40">
-        <div class="grid justify-items-center mx-30">
-          <p class="font-sans text-center text-4xl font-bold  text-blue-normal px-40">
+      <div className=" bg-white py-40">
+        <div className="grid justify-items-center mx-30">
+          <p className="font-sans text-center text-4xl font-bold  text-blue-normal px-40">
             Discover hundreds of projects to build, and connect with other
             brightminded individuals.
           </p>
 
-          <div class="pt-20 px-20">
-            <p class="font-sans text-3xl font-bold  text-black ">
+          <div className="pt-20 px-20">
+            <p className="font-sans text-3xl font-bold  text-black ">
               Featured Projects
             </p>
-            <div class="flex flex-wrap content-center">
+            <div className="flex flex-wrap content-center">
               <ProjectCard />
               <ProjectCard />
 
               <ProjectCard />
             </div>
           </div>
-          <button class=" font-sans  px-4 py-2 text-white bg-green-normal rounded-full shadow-md">
+          <button className=" font-sans  px-4 py-2 text-white bg-green-normal rounded-full shadow-md">
             Explore more projects{" "}
           </button>
 
-          <div class="pt-20 px-20">
-            <p class="font-sans text-3xl font-bold  text-black ">Profiles</p>
-            <div class="flex flex-wrap content-center">
+          <div className="pt-20 px-20">
+            <p className="font-sans text-3xl font-bold  text-black ">Profiles</p>
+            <div className="flex flex-wrap content-center">
               <ProfileCard />
               <ProfileCard />
 
               <ProfileCard />
             </div>
           </div>
-          <button class=" font-sans  px-4 py-2 text-white bg-green-normal rounded-full shadow-md">
+          <button className=" font-sans  px-4 py-2 text-white bg-green-normal rounded-full shadow-md">
             Explore more projects{" "}
           </button>
         </div>
       </div>
 
-      <div class=" bg-white-dark py-10">
-        <div class="grid justify-items-center mx-30">
-          <p class="font-sans text-md    px-20">Made with ❤️ by Team 4</p>
+      <div className=" bg-white-dark py-10">
+        <div className="grid justify-items-center mx-30">
+          <p className="font-sans text-md    px-20">Made with ❤️ by Team 4</p>
         </div>
       </div>
     </div>
