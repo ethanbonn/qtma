458e3ce2eb235e6700d53af13152a18535cb443b
Truman Be <trumanbe01@gmail.com>
2021-12-06 17:14:14 -0500

    Created a simple button handler taking in the input fields and using the fields to query data from MongoDB

--- components/Cards/QueryCard.tsx
+++ components/Cards/QueryCard.tsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
+import queryDB from "../../functions/server/queryDB";
+
+const buttonHandler = async (relationship_type: "sponsor" | "collaborator" | undefined, skills: [string] | undefined, tags: [string] | undefined) => {
+  const response = await queryDB(relationship_type, tags, skills)
+  .then(response => response);
+
+  console.log(response);
+
+}
+
 export default function Card() {
+  const [relationship_type, set_relationship_type] = useState("");
+  const [skill, set_skill] = useState("");
+  const [tag, set_tag] = useState("");
+
   return (
     <div className=" p-4 m-10 max-w-auto mx-auto bg-white rounded-xl shadow-lg flex  ">
       <div className="flex flex-wrap content-center">
         <div className="mx-2">
           <div className="text-xl font-medium text-black">I'm looking for </div>
-          <input className="my-3 border border-black rounded-lg  px-1 " />
+          <input className="my-3 border border-black rounded-lg  px-1 " onChange={(e) => set_relationship_type(e.target.value)}/>
         </div>
         <div className="mx-2">
           <div className="text-xl font-medium text-black">that are skilled in </div>
-          <input className="my-3 border border-black rounded-lg  px-1 " />
+          <input className="my-3 border border-black rounded-lg  px-1 " onChange={(e) => set_skill(e.target.value)}/>
         </div>
         <div className="mx-2">
           <div className="text-xl font-medium text-black">to build </div>
-          <input className="my-3 border border-black rounded-lg  px-1 " />
+          <input className="my-3 border border-black rounded-lg  px-1 " onChange={(e) => set_tag(e.target.value)}/>
         </div>
 
         <div>
         <div className="text-xl font-medium text-black"> </div>
-          <button className="px-3 py-1 mt-9 text-white bg-green-normal rounded-lg">
+          <button className="px-3 py-1 mt-9 text-white bg-green-normal rounded-lg" onClick={(e) => {buttonHandler(relationship_type, skill, tag)}}>
             S
           </button>
         </div>
