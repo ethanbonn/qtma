d9beb6c1f1634410ea9b1f1ba6ec07e710aeeaa8
Truman Be <trumanbe01@gmail.com>
2021-12-06 21:55:35 -0500

    Added mapping of the ProjectCard component provided a project's name and skills

--- components/Cards/ProjectCard.tsx
+++ components/Cards/ProjectCard.tsx
@@ -1,10 +1,11 @@
-export default function ProjectCard() {
+export default function ProjectCard({ project_name, project_skills }) {
+    
   return (
     <div className=" max-w-auto mx-10 bg-white rounded-xl shadow-lg flex m-10 ">
       <div className="max-w-sm  rounded-xl  overflow-hidden shadow-lg">
         <img className="w-80" src="https://api.time.com/wp-content/uploads/2020/06/crystalball.jpg?w=800&quality=85" alt="project" />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2 text-blue-normal">Omicron Predictor</div>
+          <div className="font-bold text-xl mb-2 text-blue-normal">{project_name}</div>
           {/* <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, Nonea! Maiores et perferendis eaque,
@@ -13,15 +14,15 @@ export default function ProjectCard() {
           <p className="text-gray-500 font-normal">Desired Skills:</p>
         </div>
         <div className="px-6  pb-2">
-          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-            Python
-          </span>
-          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-            Data Analysis
-          </span>
-          <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-            Knime
-          </span>
+          {
+            project_skills.map((e) => {
+              return (
+                <span className="inline-block bg-blue-normal rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                  {e}
+                </span>
+              );
+            })
+          }
         </div>
       </div>
     </div>
