7f9fea858f2b3af42038cf7f431402272f83f8c5
Truman Be <trumanbe01@gmail.com>
2021-12-30 23:08:50 -0500

    Modified query card so that state of the parent component is modified accordingly with the queried data

--- components/Cards/QueryCard.tsx
+++ components/Cards/QueryCard.tsx
@@ -1,18 +1,22 @@
+import React from "react";
 import { useState } from "react";
 import queryDB from "../../functions/server/queryDB";
-
-const buttonHandler = async (relationship_type: string | undefined, skills: [string] | undefined, tags: [string] | undefined) => {
-  const response = await queryDB(relationship_type, tags, skills)
-  // queryDB(relationship_type, tags, skills)
-  .then(response => response);
-  console.log("returning res in QueryCard", response)
-  return response;
-}
 
 export default function Card({ stateChanger }) {
   const [relationship_type, set_relationship_type] = useState("");
   const [skill, set_skill] = useState("");
   const [tag, set_tag] = useState("");
+
+  const [button_pressed, set_button_press] = useState(0);
+
+  React.useEffect(() => {
+    async function handler() {
+      await queryDB(relationship_type, tag, skill).then(response => response)
+      .then((res) => stateChanger(res))
+    }
+    handler();
+
+  }, [button_pressed]);
 
   return (
     <div className=" p-4 m-10 max-w-auto mx-auto bg-white rounded-xl shadow-lg flex  ">
@@ -32,7 +36,7 @@ export default function Card({ stateChanger }) {
 
         <div>
         <div className="text-xl font-medium text-black"> </div>
-          <button className="px-3 py-1 mt-9 text-white bg-green-normal rounded-lg" onClick={() => stateChanger(buttonHandler(relationship_type, skill, tag))}>
+          <button className="px-3 py-1 mt-9 text-white bg-green-normal rounded-lg" onClick={() => set_button_press(button_pressed + 1)}>
             S
           </button>
         </div>
