348958b8725f9d18b7c9452f0fda4bf7cf49d343
jessicaa-li <55930140+jessicaa-li@users.noreply.github.com>
2022-02-27 23:20:44 -0500

    adding profile querying based on skills and user description

--- functions/server/queryDB.ts
+++ functions/server/queryDB.ts
@@ -1,59 +1,102 @@
 import type { Project } from "../../types/models";
 import baseUrl from "../../utils/baseUrl";
 
-export type supported_timezones = "ACST" | "AEST" | "AKST" | "AST" | "AWST" | "CET" | "CST" | "EET" | "EST" | "MST" | "PST" | "WET";
+export type supported_timezones =
+  | "ACST"
+  | "AEST"
+  | "AKST"
+  | "AST"
+  | "AWST"
+  | "CET"
+  | "CST"
+  | "EET"
+  | "EST"
+  | "MST"
+  | "PST"
+  | "WET";
 
-const queryDB = async (relationship?: string, searchInput?: string, skillsInput?: string[] | [], timezone?: [supported_timezones]): Promise<Project[]> => {
+const queryDB = async (
+  relationship?: string,
+  searchInput?: string,
+  skillsInput?: string[] | [],
+  timezone?: [supported_timezones]
+): Promise<Project[]> => {
+  // Type guard for the query parameters
+  type queryParams = {
+    // key: keyof typeof String;
+    author_timezone: supported_timezones | supported_timezones[] | undefined;
+    desired_relationship_type: string | undefined;
+    search: string | undefined;
+    skills: [] | string[] | undefined;
+  };
 
-    // Type guard for the query parameters
-    type queryParams = {
-        // key: keyof typeof String;
-        author_timezone: supported_timezones | supported_timezones[] | undefined;
-        desired_relationship_type: string | undefined;
-        search: string | undefined;
-        skills: [] | string[] | undefined;
-    };
+  // Set the value of an object to the provided parameters to the function
+  const params: queryParams = {
+    author_timezone: timezone,
+    desired_relationship_type: relationship,
+    search: searchInput,
+    skills: skillsInput,
+  };
 
-    // Set the value of an object to the provided parameters to the function
-    var params: queryParams = {
-        author_timezone: timezone,
-        desired_relationship_type: relationship,
-        search: searchInput,
-        skills: skillsInput
-    };
+  // Create a separate array containing all valid parameters
+  const projectParams: Array<String | String[]> = [];
+  const profileParams = [];
 
-    // Create a separate array containing all valid parameters
-    var valid_params: Array<String | String[]> = [];
+  // // Map each parameter to the projectParams list if they are not undefined
+  // Object.keys(params).forEach(key => {
+  //     if (params[key as keyof queryParams] !== undefined && params[key as keyof queryParams] !== "" && params[key as keyof queryParams] !== []) {
+  //         console.log("Skills", params[key as keyof queryParams], typeof(params[key as keyof queryParams]));
+  //         projectParams.push(key + "=" + params[key as keyof queryParams]);
+  //     }
+  // });
 
-    // // Map each parameter to the valid_params list if they are not undefined
-    // Object.keys(params).forEach(key => {
-    //     if (params[key as keyof queryParams] !== undefined && params[key as keyof queryParams] !== "" && params[key as keyof queryParams] !== []) {
-    //         console.log("Skills", params[key as keyof queryParams], typeof(params[key as keyof queryParams]));
-    //         valid_params.push(key + "=" + params[key as keyof queryParams]);
-    //     }
-    // });
+  if (relationship) {
+    projectParams.push(
+      `desired_relationship_type=${params.desired_relationship_type}`
+    );
+  }
 
-    if(relationship) valid_params.push("desired_relationship_type=" + params.desired_relationship_type);
+  if (searchInput) {
+    projectParams.push(`search=${params.search}`);
+    profileParams.push(`search=${params.search}`);
+  }
 
-    if(searchInput) valid_params.push("search=" + params.search);
+  if (skillsInput && skillsInput.length !== 0) {
+    projectParams.push(`skills=${params.skills}`);
+    profileParams.push(`skills=${params.skills}`);
+  }
+  // If there are valid parameters, join them with &, otherwise, set the query string to query
+  const projectQueryString =
+    projectParams.length !== 0 ? projectParams.join("&") : "query";
+  const profileQueryString =
+    profileParams.length !== 0 ? profileParams.join("&") : "query";
+  console.log(
+    "BASE URL",
+    baseUrl,
+    `\n${projectQueryString}`,
+    `\n${profileQueryString}`
+  );
 
-    if(skillsInput && skillsInput.length !== 0) valid_params.push("skills=" + params.skills);
+  const projects = await fetch(
+    `${baseUrl}/api/projects/${projectQueryString}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((projects) => projects.data);
+  console.log(projects);
 
-
-    // If there are valid parameters, join them with &, otherwise, set the query string to query
-    var query_string = (valid_params.length !== 0) ? valid_params.join("&") : "query";
-    console.log("BASE URL", baseUrl);
-    var projects = await fetch(`${baseUrl}/api/projects/${query_string}`, {
-      method: 'GET'
-    })
-        .then((response) => response.json())
-        .then((projects) => {
-            return projects.data;
-        });
-    console.log(projects);
-    return projects;
-
-
+  const users = await fetch(
+    `${baseUrl}/api/users/search/${profileQueryString}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((users) => users.data);
+  console.log(users);
+  return projects;
 };
 
 export default queryDB;
